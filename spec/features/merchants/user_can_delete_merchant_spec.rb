49472dc20056d343daf717fb1ef0354cd2aaf619
lilwillifo <margaretwilliford4@gmail.com>
2018-03-14 08:54:10 -0600

    can delete a merchant from the index

--- spec/features/merchants/user_can_delete_merchant_spec.rb
+++ spec/features/merchants/user_can_delete_merchant_spec.rb
@@ -2,9 +2,11 @@ RSpec.describe 'User' do
   describe 'visits merchants index page' do
     it 'can delete a merchant' do
       Merchant.create(name: 'Ian')
-      Merchant.create(name: 'Ali')
 
       visit '/merchants'
+      click_button 'delete'
+      expect(current_path).to eq('/merchants')
+      expect(Merchant.count).to eq(0)
     end
   end
   describe 'visits a specific merchant page' do
@@ -13,7 +15,7 @@ RSpec.describe 'User' do
       Merchant.create(name: 'Ali')
 
       visit 'merchants/2'
-      click_button 'delete'
+      click_button 'Delete'
 
       expect(current_path).to eq('/merchants')
       expect(Merchant.count).to eq(1)
