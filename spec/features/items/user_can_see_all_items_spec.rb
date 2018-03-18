61b416846378e47ca4e49bc211a4dd5ee3bcdc39
lilwillifo <margaretwilliford4@gmail.com>
2018-03-18 11:03:17 -0600

    edit invoice dropdown working

--- spec/features/items/user_can_see_all_items_spec.rb
+++ spec/features/items/user_can_see_all_items_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe 'User' do
       expect(page).to have_content('Ian')
       expect(page).to have_content(4.55)
     end
+    it 'can navigate to the items dashboard' do
+      Item.create(title: 'Ian', description: 'help', price: 4.55, image: 'google.com')
+
+      visit '/items'
+      click_link('Items Dashboard')
+      expect(current_path).to eq('/items-dashboard')
+    end
   end
 end
