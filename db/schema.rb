9279aaa6a99b53a6ff5a970422164c8a261f4abc
lilwillifo <margaretwilliford4@gmail.com>
2018-03-18 11:58:21 -0600

    add merchant id foreign key to items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316221454) do
+ActiveRecord::Schema.define(version: 20180318175605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180316221454) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
