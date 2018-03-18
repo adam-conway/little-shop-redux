e8c0ae4a8678f325dd531e082b3a52e583cffa32
lilwillifo <margaretwilliford4@gmail.com>
2018-03-18 12:24:39 -0600

    merchants dashboard avg price

--- spec/features/merchants/user_visits_merchants_dashboard_spec.rb
+++ spec/features/merchants/user_visits_merchants_dashboard_spec.rb
@@ -7,13 +7,21 @@ RSpec.describe 'User' do
       item3 = Item.create(title: 'Sal', description: 'kewl', price: 20, image: 'url', merchant_id: 2)
 
       visit '/merchants-dashboard'
+
       expect(page).to have_content(merchant.name)
       expect(page).to have_content(item1.title)
       expect(page).to have_content(item2.title)
       expect(page).to_not have_content(item3.title)
     end
     it 'can see average price of item for each merchant' do
+      merchant = Merchant.create(name: 'Margaret')
+      item1 = Item.create(title: 'Ali', description: 'da bomb', price: 10, image: 'url', merchant_id: 1)
+      item2 = Item.create(title: 'Ian', description: 'canadian', price: 20, image: 'url', merchant_id: 1)
+      item3 = Item.create(title: 'Sal', description: 'kewl', price: 30, image: 'url', merchant_id: 2)
 
+      visit '/merchants-dashboard'
+      save_and_open_page
+      expect(page).to have_content(15)
     end
     it 'can see total price of all items for each merchant' do
 
