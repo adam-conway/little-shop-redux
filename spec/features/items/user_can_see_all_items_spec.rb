8c8b231b2950575965188eed0d3d80001dad4733
adam-conway <adam.n.conway@gmail.com>
2018-03-20 12:14:02 -0600

    Added tests for item index page

--- spec/features/items/user_can_see_all_items_spec.rb
+++ spec/features/items/user_can_see_all_items_spec.rb
@@ -15,5 +15,22 @@ RSpec.describe 'User' do
       click_link('Items Dashboard')
       expect(current_path).to eq('/items-dashboard')
     end
+    it 'can navigate to the create new item page' do
+      Item.create(title: 'Ian', description: 'help', price: 4.55, image: 'google.com')
+
+      visit '/items'
+      click_on('Create A New Item')
+
+      expect(current_path).to eq('/items/new')
+    end
+    it 'can delete item from page' do
+      Item.create(title: 'Ian', description: 'help', price: 4.55, image: 'google.com')
+
+      visit '/items'
+      click_on('delete')
+
+      expect(current_path).to eq('/items')
+      expect(page).to_not have_content('Ian')
+    end
   end
 end
