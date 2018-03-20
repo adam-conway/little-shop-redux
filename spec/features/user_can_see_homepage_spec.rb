deff0625b809462e802f8d2b878abbf6c8084ea9
adam-conway <adam.n.conway@gmail.com>
2018-03-19 20:19:44 -0600

    Working on the invoice dashboard

--- spec/features/user_can_see_homepage_spec.rb
+++ spec/features/user_can_see_homepage_spec.rb
@@ -1,9 +1,9 @@
-atRSpec.describe 'User' do
+RSpec.describe 'User' do
   describe 'visits an homepage' do
     it 'can see homepage content' do
       visit "/"
 
       expect(page).to have_content("M & A's LittleShop")
     end
-end
+  end
 end
