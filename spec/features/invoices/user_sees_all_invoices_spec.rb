158ac4af65144e0274b14133a1e82dc57b6c8ca2
adam-conway <adam.n.conway@gmail.com>
2018-03-20 08:57:02 -0600

    Fixed tests for some item pages

--- spec/features/invoices/user_sees_all_invoices_spec.rb
+++ spec/features/invoices/user_sees_all_invoices_spec.rb
@@ -4,8 +4,19 @@ RSpec.describe 'User' do
       Invoice.create(status: 'pending', merchant_id: 1)
       Invoice.create(status: 'shipped', merchant_id: 2)
 
+      visit '/invoices'
+      expect(page).to have_content(1)
+      expect(page).to have_content(2)
+
+    end
+    it 'can navigate to invoices dashboard' do
+      Invoice.create(status: 'pending', merchant_id: 1)
+      Invoice.create(status: 'shipped', merchant_id: 2)
 
       visit '/invoices'
+      click_on 'Invoices Dashboard'
+
+
       expect(page).to have_content(1)
       expect(page).to have_content(2)
 
